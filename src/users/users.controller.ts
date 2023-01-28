401ad39e1cd170b8ad8bbafbf0965e1f028e7f65
hitendrasinh-Parmar <hitrajput098@gmail.com>
2023-01-28 14:46:54 +0530

    user auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Patch, Put, Param, Query, NotFoundException } from '@nestjs/common';
+import { Delete } from '@nestjs/common/decorators';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUsersDto } from './dtos/update-user.dto';
+@Controller('auth')
+export class UsersController {
+  constructor(private userService: UsersService) { }
 
-@Controller('users')
-export class UsersController {}
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.userService.create(
+      body.email, body.password
+    )
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = this.userService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id))
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUsersDto) {
+    return this.userService.update(parseInt(id), body)
+  }
+}
--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { AfterInsert, AfterUpdate, AfterRemove, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 
 @Entity()
@@ -11,4 +11,19 @@ export class Users {
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted user with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated user with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed user with id', this.id);
+  }
 }
